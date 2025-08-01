99eec474df0a90f70aa02263416de9722415fdf4
Pranali <pranali.gharat@routemobile.com>
2025-08-01 17:17:34 +0530

    changes 1

--- TEST1/test_py.py
+++ TEST1/test_py.py
@@ -23,4 +23,4 @@ print("encoded_cipher_text",encoded_cipher_text)
 #url = 'http://localhost:1818//rmlwebsitelead/create/v1/'
 #header = {'Api-Key' : 'wZW5kaW5nJywgJ2lzX2xpdmUnOiBUcnV'}
 res = requests.post(url, headers = header,data=data)
-print("res",res.text)
+print("res11111111",res.text)
